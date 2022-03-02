167f265eb2ee9e8168af45147792c748c9f71ca5
Doğukan Korkut <doguuu1998@hotmail.com>
2022-03-02 15:53:47 +0300

    Add style for note card

--- note-app/src/App.vue
+++ note-app/src/App.vue
@@ -1,5 +1,17 @@
 <template>
-  <h3>selam</h3>
+  <div class="app">
+    <NoteCard></NoteCard>
+  </div>
 </template>
 <script setup>
+import NoteCard from './components/NoteCard/NoteCard.vue';
 </script>
+<style lang="scss">
+@import url("https://fonts.googleapis.com/css2?family=Nunito+Sans:wght@300;400;700;900&display=swap");
+.app {
+  width: 100%;
+  height: 100vh;
+  background-color: darkcyan;
+  font-family: "Nunito Sans", sans-serif;
+}
+</style>
---
+++ note-app/src/components/NoteCard/NoteCard.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="note-card">
+    <div class="note-card-title">Note App</div>
+    <div class="note-card-content">
+      <form class="note-card-content-form">
+        <input placeholder="Title" type="text" class="note-card-content-form-input" />
+        <textarea placeholder="Description" class="note-card-content-form-textarea"></textarea>
+      </form>
+      <div class="note-card-content-footer">
+        <div class="note-card-content-footer-colors">
+          <label class="note-card-content-footer-colors-label red">
+            <input type="radio" class="note-card-content-footer-colors-label-input" />
+          </label>
+          <label class="note-card-content-footer-colors-label blue">
+            <input type="radio" class="note-card-content-footer-colors-label-input" />
+          </label>
+          <label class="note-card-content-footer-colors-label yellow">
+            <input type="radio" class="note-card-content-footer-colors-label-input" />
+          </label>
+          <label class="note-card-content-footer-colors-label black">
+            <input type="radio" class="note-card-content-footer-colors-label-input" />
+          </label>
+        </div>
+        <button type="button" class="note-card-content-footer-button">Add Note</button>
+      </div>
+    </div>
+  </div>
+</template>
+<script setup>
+</script>
+<style src="./NoteCard.scss" lang="scss">
+</style>
