b6d8e35080d593337c0d828e44eccdc71d596b8c
Doğukan Korkut <doguuu1998@hotmail.com>
2022-03-02 21:34:09 +0300

    Add a note with the color and message the user wants

--- note-app/src/components/NoteCard/NoteCard.vue
+++ note-app/src/components/NoteCard/NoteCard.vue
@@ -1,10 +1,19 @@
 <template>
   <div class="note-card">
-    <div class="note-card-title">Note App</div>
+    <h1 class="note-card-title">Note App</h1>
     <div class="note-card-content">
       <form class="note-card-content-form">
-        <input placeholder="Title" type="text" class="note-card-content-form-input" />
-        <textarea placeholder="Description" class="note-card-content-form-textarea"></textarea>
+        <input
+          placeholder="Title"
+          type="text"
+          class="note-card-content-form-input"
+          v-model="noteTitle"
+        />
+        <textarea
+          placeholder="Description"
+          class="note-card-content-form-textarea"
+          v-model="noteDescription"
+        ></textarea>
       </form>
       <div class="note-card-content-footer">
         <div class="note-card-content-footer-colors">
@@ -37,15 +46,20 @@
             <input type="radio" class="note-card-content-footer-colors-label-input" />
           </label>
         </div>
-        <button type="button" class="note-card-content-footer-button">Add Note</button>
+        <button @click="addNote" type="button" class="note-card-content-footer-button">Add Note</button>
       </div>
     </div>
+    <NoteList @deleteCard="deleteCard" :noteList="noteList"></NoteList>
   </div>
 </template>
 <script setup>
+import NoteList from "../NoteList/NoteList.vue";
 import { ref } from "vue";
 
 const selectedColor = ref(null);
+const noteTitle = ref(null);
+const noteDescription = ref(null);
+const noteList = ref([]);
 
 const selectRed = () => {
   selectedColor.value = "red";
@@ -59,6 +73,32 @@ const selectYellow = () => {
 const selectGrey = () => {
   selectedColor.value = "grey";
 }
+
+const deleteCard = (card) => {
+  noteList.value = noteList.value.filter(c => c != card);
+}
+
+const addNote = () => {
+  if (noteTitle.value != null && noteDescription.value != null && selectedColor.value != null) {
+    let dateObj = new Date();
+    let month = dateObj.getUTCMonth() + 1; //months from 1-12
+    let day = dateObj.getUTCDate();
+    let year = dateObj.getUTCFullYear();
+    const noteContent = ref({
+      title: noteTitle.value,
+      desc: noteDescription.value,
+      color: selectedColor.value,
+      time: year + "/" + month + "/" + day
+    });
+    noteList.value.push(noteContent.value);
+    noteTitle.value = null;
+    noteDescription.value = null;
+    selectedColor.value = null;
+    return;
+  }
+  alert("Bütün verileri giriniz!");
+}
+
 </script>
 <style src="./NoteCard.scss" lang="scss">
 </style>
---
+++ note-app/src/components/NoteList/NoteList.vue
@@ -0,0 +1,34 @@
+<template>
+  <transition name="card-list-transition">
+    <div v-show="props.noteList.length > 0" class="card-list">
+      <div
+        v-for="(card, index) in props.noteList"
+        :key="index"
+        :style="cardBackgroundColor(card.color)"
+        class="card-list-item"
+      >
+        <h3 class="card-list-item-title">{{ card.title }}</h3>
+        <p class="card-list-item-desc">{{ card.desc }}</p>
+        <span class="card-list-item-time">{{ card.time }}</span>
+        <span @click="$emit('deleteCard', card)" class="card-list-item-close-icon">
+          <i class="fa-solid fa-xmark"></i>
+        </span>
+      </div>
+    </div>
+  </transition>
+  <div v-show="!(props.noteList.length > 0)" class="none-card-list">
+    <p class="none-card-list-message">No notes yet</p>
+  </div>
+</template>
+<script setup>
+import { defineProps } from 'vue';
+
+const props = defineProps(['noteList']);
+
+const cardBackgroundColor = (color) => {
+  return `background-color: ${color};`
+}
+
+</script>
+<style src="./NoteList.scss" lang="scss">
+</style>
